29cd823c51fe192139baf0d9fdc64bad49858d56
goodhamgupta <shubhamg2208@live.com>
2024-01-24 03:00:03 +0800

    Remove default phoenix content

--- lib/coaxis_web/components/button_component.ex
+++ lib/coaxis_web/components/button_component.ex
@@ -10,7 +10,10 @@ defmodule CoaxisWeb.ButtonComponent do
       class="bg-mono-black rounded-md pt-1.5 pr-[29px] pb-1.5 pl-[29px] flex flex-row gap-2.5 items-center justify-center shrink-0 w-[136px] h-9 relative"
       phx-click={@click_event}
     >
-      <span class="text-mono-white text-center font-['OpenSans-Bold',_sans-serif] text-base font-bold relative">
+      <span
+        class="text-mono-white text-center font-['OpenSans-Bold',_sans-serif] text-base font-bold relative"
+        style="white-space: nowrap;"
+      >
         <%= @text %>
       </span>
     </button>
