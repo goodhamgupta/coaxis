6f6755b12903a964fcacfa3e75c1443be31367ec
goodhamgupta <shubhamg2208@live.com>
2024-01-27 12:41:29 +0800

    Add project interest seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-personal_interests = [
+engagement_interests = [
   %{name: "fundraising", description: "I want to raise money for my project", type: :engagement},
   %{
     name: "support_impact_ventures",
@@ -34,8 +34,37 @@ personal_interests = [
   }
 ]
 
-for personal_interest <- personal_interests do
+for engagement_interest <- engagement_interests do
   Coaxis.Accounts.Resources.Interest
-  |> Ash.Changeset.for_create(:create, personal_interest)
+  |> Ash.Changeset.for_create(:create, engagement_interest)
+  |> Coaxis.Accounts.create!()
+end
+
+project_interests = [
+  %{name: "agriculture", description: "Agriculture", type: :project},
+  %{name: "air", description: "Air", type: :project},
+  %{
+    name: "biodiversity_and_econsystems",
+    description: "Biodiversity & Ecosystems",
+    type: :project
+  },
+  %{name: "climate", description: "Climate", type: :project},
+  %{name: "diversity_inclusion", description: "Diversity & Inclusion", type: :project},
+  %{name: "education", description: "Education", type: :project},
+  %{name: "employment", description: "Employment", type: :project},
+  %{name: "energy", description: "Energy", type: :project},
+  %{name: "financial_services", description: "Financial Services", type: :project},
+  %{name: "health", description: "Health", type: :project},
+  %{name: "land", description: "Land", type: :project},
+  %{name: "oceans_and_coastal_zones", description: "Oceans and Coastal Zones", type: :project},
+  %{name: "pollution", description: "Pollution", type: :project},
+  %{name: "real_estate", description: "Real Estate", type: :project},
+  %{name: "waste", description: "Waste", type: :project},
+  %{name: "water", description: "Water", type: :project}
+]
+
+for project_interest <- project_interests do
+  Coaxis.Accounts.Resources.Interest
+  |> Ash.Changeset.for_create(:create, project_interest)
   |> Coaxis.Accounts.create!()
 end
